72068f871f6131af56a45a3ce03b5fc9973c1a8b
YoshinoriN <nayo-msoft@outlook.jp>
2018-07-08 21:21:20 +0900

    Delete trailing semicolon

--- src/main/scala/net/yoshinorin/gitbucket/logs/controllers/LogsController.scala
+++ src/main/scala/net/yoshinorin/gitbucket/logs/controllers/LogsController.scala
@@ -10,14 +10,14 @@ class LogsController extends ControllerBase with AdminAuthenticator {
   val os = net.yoshinorin.gitbucket.logs.services.operatingsystem.OperatingSystem.getInstance
 
   get("/admin/logs")(adminOnly {
-    redirect(s"/admin/logs/logback");
+    redirect(s"/admin/logs/logback")
   })
 
   get("/admin/logs/logback")(adminOnly {
     net.yoshinorin.gitbucket.logs.html.logback(
       LogBack.logBackSettingsFile,
       LogBack.getLogBackSettings
-    );
+    )
   })
 
   get("/admin/logs/gitbucketlog")(adminOnly {
@@ -27,12 +27,12 @@ class LogsController extends ControllerBase with AdminAuthenticator {
     if (Try(lineNum.toInt).toOption != None) {
       val n = lineNum.toInt
       if (n > defaultSettings.displayLimitLines) {
-        net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(defaultSettings.displayLimitLines));
+        net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(defaultSettings.displayLimitLines))
       } else {
-        net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(n));
+        net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(n))
       }
     } else {
-      net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(defaultSettings.defaultDisplayLines));
+      net.yoshinorin.gitbucket.logs.html.gitbucketlog(defaultSettings, os.getLog(defaultSettings.defaultDisplayLines))
     }
   })
 }
