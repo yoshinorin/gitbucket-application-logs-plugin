f8c1c360ed564d28373744a36fc1d1a728c42aee
YoshinoriN <nayo-msoft@outlook.jp>
2018-07-22 22:30:09 +0900

    Implement get all logfile path method

--- src/main/scala/net/yoshinorin/gitbucket/applicationlogs/services/LogBackService.scala
+++ src/main/scala/net/yoshinorin/gitbucket/applicationlogs/services/LogBackService.scala
@@ -3,6 +3,11 @@ package net.yoshinorin.gitbucket.applicationlogs.services
 import java.nio.file.{Files, Paths}
 import scala.collection.JavaConverters._
 import scala.xml.XML
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import org.slf4j.LoggerFactory
 import gitbucket.core.util.StringUtil
 import net.yoshinorin.gitbucket.applicationlogs.models.LogBackInfo
 import net.yoshinorin.gitbucket.applicationlogs.utils.Error
@@ -11,6 +16,30 @@ trait LogBackService {
 
   val enableLogging = System.getProperties().asScala.toMap.contains("logback.configurationFile")
   val confPath = System.getProperties().asScala.toMap.getOrElse("logback.configurationFile", Error.NOT_FOUND_LOGBACK_SETTINGS.message)
+
+  private val ctx = org.slf4j.LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+
+  def getLogFilePaths: Option[List[String]] = {
+    import scala.collection.JavaConversions._
+
+    var paths = List[String]()
+    for (logger <- ctx.getLoggerList) {
+      val i = logger.iteratorForAppenders
+      while ({
+        i.hasNext
+      }) {
+        val appender = i.next
+        if (appender.isInstanceOf[FileAppender[_]] || appender.isInstanceOf[RollingFileAppender[_]]) {
+          paths :+= appender.asInstanceOf[FileAppender[ILoggingEvent]].getFile
+        }
+      }
+    }
+    if (paths.size() != 0) {
+      Some(paths)
+    } else {
+      None
+    }
+  }
 
   val logBackSettingsFile: Option[String] = {
     if (enableLogging) {
