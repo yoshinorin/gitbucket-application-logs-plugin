e57132bc57d63800c4bb9afaeccbfcba2d4c7987
YoshinoriN <nayo-msoft@outlook.jp>
2018-07-26 02:26:36 +0900

    readLog function more functional

--- src/main/scala/net/yoshinorin/gitbucket/applicationlogs/services/ApplicationLogService.scala
+++ src/main/scala/net/yoshinorin/gitbucket/applicationlogs/services/ApplicationLogService.scala
@@ -2,34 +2,27 @@ package net.yoshinorin.gitbucket.applicationlogs.services
 
 import java.io.File
 import java.nio.charset.Charset
-import scala.collection.mutable.ArrayBuffer
 import scala.util.Try
 import org.apache.commons.io.input.ReversedLinesFileReader
+import net.yoshinorin.gitbucket.applicationlogs.models.LogFile
 
 trait ApplicationLogService {
 
   val defaultDisplayLines: Int = 1000
   val displayLimitLines: Int = 10000
 
-  def readLog(logfile: LogFile, n: Int = defaultDisplayLines): Try[Option[ArrayBuffer[String]]] = Try {
-
+  def readLog(logfile: LogFile, n: Int = defaultDisplayLines): Try[Option[List[String]]] = Try {
     val logFile = new File(logfile.path)
     if (!logFile.exists()) {
       None
     }
 
-    val r = new ReversedLinesFileReader(new File(logfile.path), Charset.defaultCharset())
+    val reversedReader = new ReversedLinesFileReader(new File(logfile.path), Charset.defaultCharset())
     try {
-      var counter: Int = 0
-      var line: String = ""
-      var lines = ArrayBuffer[String]()
-      while ({ line = r.readLine(); line != null } && counter <= n) {
-        lines += line
-        counter += 1
-      }
-      Some(lines.reverse)
+      val counter = Iterator.from(0)
+      Some(Iterator.continually(reversedReader.readLine()).takeWhile(_ != null && { counter.next < n }).toList.reverse)
     } finally {
-      if (r != null) r.close()
+      if (reversedReader != null) reversedReader.close()
     }
   }
 
