4841697f19e20d71eca90599cda8c93c6164bd54
YoshinoriN <nayo-msoft@outlook.jp>
2018-08-02 23:08:42 +0900

    Delete unnecessary object

--- src/main/scala/net/yoshinorin/gitbucket/applicationlogs/utils/Error.scala
+++ src/main/scala/net/yoshinorin/gitbucket/applicationlogs/utils/Error.scala
@@ -4,11 +4,8 @@ sealed abstract class Error(val message: String)
 
 object Error {
 
-  object NOT_SUPPORTED extends Error("NOT SUPPORTED")
   object FAILURE extends Error("ERROR")
-  object DISABLE extends Error("Log setting is disable.")
   object NOT_FOUND_LOGBACK_SETTINGS extends Error("Can not find logback configuration file")
-  object NOT_CONFIGURE extends Error("Dosen't configure Logback")
   object FILE_NOT_FOUND extends Error("FILE NOT FOUND")
 
 }
