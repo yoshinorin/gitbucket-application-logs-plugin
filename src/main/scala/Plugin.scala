3d9be27e69ee5210571ce0f4e7d69c34e89ce7d5
YoshinoriN <nayo-msoft@outlook.jp>
2019-08-12 10:46:07 +0900

    refactor: add return type annotation

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -11,13 +11,15 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("0.1.0"),
     new Version("0.2.0"),
     new Version("1.0.0"),
+    new Version("2.0.0"),
+    new Version("3.0.0")
   )
 
   override val systemSettingMenus: Seq[(Context) => Option[Link]] = Seq(
     (context: Context) => Some(Link("application-logs", "Application Logs", "admin/application-logs", Some("file-text")))
   )
 
-  override val controllers = Seq(
+  override val controllers: Seq[(String, ApplicationLogsController)] = Seq(
     "/*" -> new ApplicationLogsController
   )
 }
